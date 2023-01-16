526619e65291375c6215dfc205420967a9c7d762
mostafarmstu <mostafarmstu@gmail.com>
2023-01-16 23:54:16 +0600

    audio added

--- src/components/ChannelItem.js
+++ src/components/ChannelItem.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { AiOutlineRight } from "react-icons/ai";
+import channelClickSound from "../media/iconSound.wav";
 
 const ChannelItem = ({ text }) => {
+    const audio = new Audio(channelClickSound)
+    const handleChannelClick = () => {
+        audio.play();
+    }
   return (
-    <div className="flex w-full font-bold float-left text-green-400 hover:text-gray-200 text-lg hover:bg-green-600 transition-colors duration-300">
+    <div className="flex w-full font-bold float-left text-green-400 hover:text-gray-200 text-lg hover:bg-green-600 transition-colors duration-300" onClick={handleChannelClick}>
       <button
         type="button"
         className="border-solid border-2 border-green-600 w-full p-2 flex items-center focus:bg-green-600 "
--- src/components/SidebarIcons.js
+++ src/components/SidebarIcons.js
@@ -1,8 +1,15 @@
 import React from "react";
+import clickableSound from "../media/client_public_sound_click.mp3";
 
 const SidebarIcons = ({ icon, text = "tooltips", setChannel }) => {
+    const audio = new Audio(clickableSound);
+
+    const handleIconClick = () => {
+        setChannel(text)
+        audio.play();
+    }
   return (
-    <div className="sidebar-icon group" onClick={() => setChannel(text)}>
+    <div className="sidebar-icon group" onClick={handleIconClick}>
       {icon}
       <span className="sidebar-tooltip group-hover:scale-100">{text.toUpperCase()}</span>
     </div>
